38bb32a9cd49317e434d293bf1fae4a6737c717a
pabina <rimalpabina1234@gmail.com>
2022-06-16 23:20:56 +0545

    useState practice done

---
+++ src/pages/ForUseState.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "../mycss.css";
+import { useState } from "react";
+
+const ForUseState = () => {
+  const [counter, setCounter] = useState(0);
+  const [inputvalue, setinputvalue] = useState("hello");
+  const myfun = function () {
+    setCounter(counter + 1);
+  };
+  const onchangefun = function (event) {
+    let valueis = event.target.value;
+    setinputvalue(valueis);
+  };
+
+  return (
+    <>
+      <h2>increment counter</h2>
+      <div className="countsection">
+        {counter}
+        <button
+          type="button"
+          className="btn btn-primary countbtn"
+          onClick={myfun}
+        >
+          click me
+        </button>
+      </div>
+
+      <div className="mynext">
+        <h2>form control</h2>
+        <div className="inputField">
+          <input type="text" onChange={onchangefun} />
+          {inputvalue}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ForUseState;
